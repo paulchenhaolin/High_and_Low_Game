22804d34ab3c3a208837c357a90890c7e085e95b
paulchenhaolin <54451600+paulchenhaolin@users.noreply.github.com>
2021-05-06 23:59:30 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+# Solution
+#Display art
+from art import logo
+
+
+#generate a random account from the game data
+
+
+#format the account data into printable format
+
+# ask user for a guess
+
+#check if user is correct
+##get follower count of each account
+##use if statement to check if user is correct
+
+#give user feedback on their guess.
+
+#score keeping
+
+#make the game repeatable.
+
+#Making account at position B beome the next acount at position A.
+
+#clear the screen between rounds
